eff2288e40731b16516dd7ed41a929c04064f6f6
Vasishta Balla <vasishta.balla@gmail.com>
2020-11-04 23:14:55 +0530

    Initial Push

---
+++ invest-track-service/src/main/java/dev/vasishta/invest/track/InvestTrackServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.vasishta.invest.track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestTrackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestTrackServiceApplication.class, args);
+	}
+
+}
---
+++ invest-track-service/src/test/java/dev/vasishta/invest/track/InvestTrackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vasishta.invest.track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestTrackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
